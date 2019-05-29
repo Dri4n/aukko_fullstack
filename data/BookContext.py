0e3313d2ca51f85b6483884d8e69fbd053719f76
Carlos mauricio  Orostegui <carlos@MBP-de-Carlos.iptv.microsoft.com>
2019-05-29 01:12:06 -0400

    se agrega configuración adicional en creación de la conexion con sqllite

--- data/BookContext.py
+++ data/BookContext.py
@@ -4,6 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 
 db_path = os.path.join(os.path.dirname(__file__), 'bookscraper.db')
-engine = create_engine('sqlite:///{}'.format(db_path))
+engine = create_engine('sqlite:///{}'.format(db_path), encoding='utf8', convert_unicode=True)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()

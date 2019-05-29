ef717b91a764c44de54190fe1abd7ea8de3f8863
Carlos mauricio  Orostegui <carlos@MacBook-Pro-de-Carlos.local>
2019-05-29 18:37:50 -0400

    se elimina valor por defecto para propiedades de la clase book, se agregan propiedades price_tax y tax

--- scraper/models/Book.py
+++ scraper/models/Book.py
@@ -1,23 +1,24 @@
 class Book():
     def __init__ (
         self, 
-        title = None, 
-        category = None, 
-        category_url = None, 
-        thumbail = None, 
-        price = None, 
-        stock = None, 
-        description = None, 
-        upc = None
+        title, 
+        category, 
+        category_url, 
+        thumbail, 
+        price,
+        price_tax,
+        tax,
+        stock, 
+        description, 
+        upc
     ):
         self.title = title
         self.category = category
         self.category_url = category_url
         self.price = price
+        self.price_tax = price_tax
+        self.tax = tax
         self.description = description
         self.thumbail = thumbail
         self.stock = stock
         self.upc = upc
-    
-    def toString(self):
-        print('BOOK {} {}'.format(self.title, self.price))

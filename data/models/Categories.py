91699ec0472393e9f1c46e2b625db07178427129
Carlos mauricio  Orostegui <carlos@MacBook-Pro-de-Carlos.local>
2019-05-29 18:36:24 -0400

    se modifica largo maximo de columnas name y url

--- data/models/Categories.py
+++ data/models/Categories.py
@@ -7,8 +7,8 @@ class Category(Base, Serializable):
     __tablename__ = 'categories'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    url = Column(String, nullable=False, unique=True)
+    name = Column(String(50), nullable=False)
+    url = Column(String(255), nullable=False, unique=True)
     book = relationship("Book")
 
     def __init__(self, name, url):

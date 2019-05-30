71cad301e3f1b271b4c37232a68a8277b2d4edf3
Carlos mauricio  Orostegui <carlos@MBP-de-Carlos.iptv.microsoft.com>
2019-05-29 23:25:22 -0400

    se agrega opcion para serializar relaciones

--- data/models/Serialize.py
+++ data/models/Serialize.py
@@ -1,8 +1,15 @@
 from collections import OrderedDict
+from sqlalchemy.ext.declarative import DeclarativeMeta
 
 class Serializable(object):
     def _asdict(self):
         result = OrderedDict()
         for key in self.__mapper__.c.keys():
             result[key] = getattr(self, key)
+        
+        for attr, relation in self.__mapper__.relationships.items():
+            value = getattr(self, attr)
+            if value is not None:
+                if isinstance(value.__class__, DeclarativeMeta):
+                    result[relation.key] = value._asdict()
         return result
